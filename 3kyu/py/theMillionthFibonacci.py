6f537de8e0c3ee21520280cedf337c27b7859058
Adel Ansari <adelansari.a@gmail.com>
2024-02-09 21:32:19 +0200

    solved millionth fib in python

---
+++ 3kyu/py/theMillionthFibonacci.py
@@ -0,0 +1,56 @@
+'''
+https://www.codewars.com/kata/53d40c1e2f13e331fc000c26/train/javascript
+
+The year is 1214. One night, Pope Innocent III awakens to find the the archangel Gabriel floating before him. Gabriel thunders to the pope:
+
+Gather all of the learned men in Pisa, especially Leonardo Fibonacci. In order for the crusades in the holy lands to be successful, these men must calculate the millionth number in Fibonacci's recurrence. Fail to do this, and your armies will never reclaim the holy land. It is His will.
+
+The angel then vanishes in an explosion of white light.
+
+Pope Innocent III sits in his bed in awe. How much is a million? he thinks to himself. He never was very good at math.
+
+He tries writing the number down, but because everyone in Europe is still using Roman numerals at this moment in history, he cannot represent this number. If he only knew about the invention of zero, it might make this sort of thing easier.
+
+He decides to go back to bed. He consoles himself, The Lord would never challenge me thus; this must have been some deceit by the devil. A pretty horrendous nightmare, to be sure.
+
+Pope Innocent III's armies would go on to conquer Constantinople (now Istanbul), but they would never reclaim the holy land as he desired.
+
+
+In this kata you will have to calculate fib(n) where:
+
+fib(0) := 0
+fib(1) := 1
+fib(n + 2) := fib(n + 1) + fib(n)
+Write an algorithm that can handle n up to 2000000.
+
+Your algorithm must output the exact integer answer, to full precision. Also, it must correctly handle negative numbers as input.
+
+HINT I: Can you rearrange the equation fib(n + 2) = fib(n + 1) + fib(n) to find fib(n) if you already know fib(n + 1) and fib(n + 2)? Use this to reason what value fib has to have for negative values.
+
+HINT II: See https://web.archive.org/web/20220614001843/https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-11.html#%_sec_1.2.4
+'''
+
+
+def fib(n):
+    n = int(n)
+    if n >= 0:
+        return fibonacci_helper(0, 1, n)[0]
+    else:
+        return -fibonacci_helper(0, 1, -n)[0] if n % 2 == 0 else fibonacci_helper(0, 1, -n)[0]
+
+def fibonacci_helper(a, b, n):
+    if n == 0:
+        return (a, b)
+
+    half_fib, next_half_fib = fibonacci_helper(a, b, n // 2)
+
+    double_index_fib = half_fib * (2 * next_half_fib - half_fib)
+    next_double_index_fib = half_fib * half_fib + next_half_fib * next_half_fib
+
+    return (next_double_index_fib, double_index_fib + next_double_index_fib) if n % 2 == 1 else (double_index_fib, next_double_index_fib)
+
+print(fib(2000000))
+print(fib(0))  # 0
+print(fib(1))  # 1
+print(fib(2))  # 1
+print(fib(-6))  # -8
